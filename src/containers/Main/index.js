a13bd4fdebf35121c90519cbbeef65a6b3447d6c
Vlad <ebgo@mail.ru>
2020-05-06 11:31:25 +0400

    environment is ready

---
+++ src/containers/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+function Main() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Learn React
+      </header>
+    </div>
+  );
+}
+
+export default Main;
